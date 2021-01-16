68d06db2e1d103dc80913daf582a506783277734
Mgcoder9 <sgocean25@gmail.com>
2021-01-16 20:31:44 +0500

    Time&Date

--- Practice.py
+++ Practice.py
@@ -1,11 +1,14 @@
-def persnol_info(phone):
-    if phone == '03059601725':
-        name = 'Ghulam Mustafa'
-        age = 22
-        id = 3550403856881
-        blood = 'B+V'
-        edu = '12th Pass'
-        address = 'Lala Town nearby Al-minar Masjad, Fortabbas.'
-        print('%s is registered on the person name is %s and CNIC number is %s\nPerson Details:\nHe lives in %s\n%s age is %s and his Blood Group is %s.'%(phone,name,id,address,name,age,blood))
+from tqdm import tqdm
+import requests
+import os
+os.chdir('C:\\Users\\Mgcoder\\Desktop')
+url = 'http://download1078.mediafire.com/1360a1m4o2rg/93pjtme3e2qey9x/www.goeagle.pk_W3+Resources_2021.rar'
+res = requests.get(url, stream=True)
+res.raise_for_status()
 
-persnol_info('03059601725')
+total_size = int(res.headers['content-length'])
+with open('w3.rar', 'wb') as f:
+    with tqdm(total=total_size, desc='W3School Download ', unit='B', unit_divisor=1024, unit_scale=True) as pbar:
+        for chunk in res.iter_content(4096):
+            f.write(chunk)
+            pbar.update(len(chunk))
--- my.py
+++
@@ -1,1 +0,0 @@
-print('for x in range(20)')
