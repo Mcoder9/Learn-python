34bba08e722a94b8f041652641508bd0a89c0215
Mgcoder9 <sgocean25@gmail.com>
2021-01-19 16:21:17 +0500

    Selenium

--- form fillup_by_selenium.py
+++ form fillup_by_selenium.py
@@ -6,39 +6,59 @@ from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import Select
 
-browser = 'chrome'
+browser = 'firefox'
 if browser == 'chrome':
     driver = webdriver.Chrome(ChromeDriverManager().install())
 elif browser == 'firefox':
     driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 else:
     print('Give the correct path of %s'%browser)
-url = 'https://www.orangehrm.com/orangehrm-30-day-trial/'
+url = 'https://www.jqueryscript.net/demo/Drop-Down-Combo-Tree/'
 driver.get(url)
+
+
 #Facebook login
 # driver.find_element_by_css_selector('div._6lux>input#email').send_keys('03059601725')
 # driver.find_element_by_css_selector('div._6lux>input#pass').send_keys('fool_751')
 # driver.find_element_by_css_selector('div._6ltg>button#u_0_b').click()
 
-# url = 'https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html'
+
 # Dropdown practice with Select
+# url = 'https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html'
 # day_ele = driver.find_element_by_css_selector('select.form-control') # get element
 # for op in Select(day_ele).options: # loop and get options
 #     print(op.text)
 
-# url = 'https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html'
+
 # Dropdown practice with xpath (without select)
+# url = 'https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html'
 # day_ele = driver.find_elements_by_xpath('//select[@id="select-demo"]/option') # get element
 # for op in day_ele: # loop and get options
 #     print(op.text)
 
-# url = 'https://www.orangehrm.com/orangehrm-30-day-trial/'
+
 # Generic method for dropdown
-for_indus = driver.find_elements_by_xpath('//select[@id="Form_submitForm_Industry"]')
-print(len(for_indus))
-for option in for_indus:
-    print(option.text)
-    if option == 'Travel':
-        option.click()
-        break
+# url = 'https://jobs.tcs.com.pk/webresume/'
+# def select_dropdown(option_list,value):
+#     print(len(option_list))
+#     for option in option_list:
+#         if option.text == value:
+#             option.click()
+#             break
+#         print(option.text)
+# for_cont = driver.find_elements_by_xpath('//select[@id="ddlCurrCountry"]/option')
+# select_dropdown(for_cont,'India')
+
+
+# Generic method for Jquery dropdown
+# url = 'https://www.jqueryscript.net/demo/Drop-Down-Combo-Tree/'
+
+
+
+
+
+
+
+
+
 
